1d443228574c9dff6f9b5da6fec7ca74977dc243
Kim Nguyen <nnhk23@gmail.com>
2020-10-02 05:48:49 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,10 +1,25 @@
 require_relative './part_1_solution.rb'
-
+require 'pry'
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-end
+  result = {}
+  cart.each do |food|
+    food.each do |key, value|    
+     coupons.each do |coupon|
+      if food[key] == coupon[:item] && food[:count] > coupon[:num]
+        food[:count] =  food[:count] - coupon[:num]
+        if result["#{food} W/COUPON"]
+          result["#{food} W/COUPON"][:count] += 1
+        else
+          result["#{food} W/COUPON"] = {:price => coupon[:cost]/coupon[:num], :clearance => food[:clearance], :count => coupon[:num]}
+        end
+      end
+    end
+     result[key] = value
+  end
+  end
+  result
+    binding.pry
+  end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
